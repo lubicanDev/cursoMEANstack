0558b2888e2f94be10cce00154f8e88fce7e6d45
G03-EQ04\tarde <ricardo@lubican.com>
2017-01-19 20:59:20 +0100

    ejemplo para crear un servidr node

---
+++ 07__node_express/01_node_ex/servidor.js
@@ -0,0 +1,24 @@
+// NOTA: Para instalar el modulo express indicado en package.json, ejecutar npm install sobre el directorio actual
+
+// imporrtamos mucho
+var miExpress = require("express");
+
+// Instanciar
+var app = miExpress();
+
+// Enrutamiento
+app.get("/", peticionPrincipal); // cuando alquien acceda/se haga una peticion (request) a la raiz...
+
+app.get("/about", peticionAbout);
+
+function peticionPrincipal(peticion, respuesta) {
+    respuesta.sendfile(__dirname + "/publico/index.html"); // ... se le envía este fichero
+}
+
+function peticionAbout(peticion, respuesta) {
+    respuesta.sendfile(__dirname + "/publico/about.html");
+}
+
+app.listen(9000);
+
+console.log("Servidor Lanzado");
