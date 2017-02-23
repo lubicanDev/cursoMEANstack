039291daa6ac5a8f6af49af2243d663dbdd2658b
G03-EQ04\tarde <ricardo@lubican.com>
2017-02-23 20:38:19 +0100

    Angular2/my-dream-app - CAMBIOS

--- angular2/my-dream-app/src/app/app.component.ts
+++ angular2/my-dream-app/src/app/app.component.ts
@@ -4,14 +4,16 @@ import { Component } from '@angular/core';
 
 class Room {
   // Definicion de atributos
-  room_id: string;
+  room_id: number;
   isDouble: boolean;
+
   // Metodo constructor
-  constructor(room_id: string, isDouble: boolean) {
+  constructor(room_id: number, isDouble: boolean) {
     this.room_id = room_id;
     this.isDouble = isDouble;
   }
   //Metodos
+  // para transformar true/false en Doble/Simple
   getRoomType(): string {
 
     // return isDouble? 'doble' : 'Sencilla';
@@ -29,16 +31,26 @@ class Customer {
   customer_id: string;
   name: string;
   email: string;
-  //Metod constructor
+
+  //Metodo constructor
   constructor(customer_id: string, name: string, email: string) {
     this.customer_id = customer_id;
     this.name = name;
     this.email = email;
   }
   //Metodos
-
+  // clone() -> con esto devolvemos el objeto de una variable
+  clone(): Customer {
+    return new Customer(this.customer_id, this.name, this.email);
+  }
 }
 
+var arrayRooms = [
+  new Room(101, true),
+  new Room(107, false),
+  new Room(201, true),
+  new Room(301, false)
+];
 
 @Component({
   selector: 'app-root',
@@ -55,21 +67,20 @@ export class AppComponent {
     room2 = new Room('301', false);*/
 
   // Otra Forma   
-  rooms = [
-    new Room('101', true),
-    new Room('107', false),
-    new Room('201', true),
-    new Room('301', false)
-  ];
+  rooms: Room[] = arrayRooms;
+
+  //CUSTOMERS
+  
   customers: Customer[] = [];
-  customerSelected: Customer = new Customer("", "", "")
+  customerSelected: Customer = new Customer("", "", "");
 
   //Metodos del componente
-  selectCustomer(customer: Customer) {
-
+  selectCustomer(customer: Customer):void {
+    this.customerSelected = customer;
   }
   addCustomer(customer: Customer) {
-
+    this.customers.push(customer.clone()); // no podemos utilizar una variable, hay que usar el objeto al que apunta la variable. por eso utilizamos el método ".clone()"
+    this.customerSelected = new Customer("","","");
   }
 
 
